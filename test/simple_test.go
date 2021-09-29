521084fb1c097cd231605435184240921300d3d1
ayildirimnec <ahmet.yildirim@nec.com.au>
2021-09-29 13:36:32 +1000

    added rg actual check

--- test/simple_test.go
+++ test/simple_test.go
@@ -3,6 +3,9 @@ package test
 import (
 	"testing"
 
+	"os"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -27,4 +30,9 @@ func TestSimpleTerraform(t *testing.T) {
 		test2_output := terraform.Output(t, terraformOptions, "rg_location")
 		assert.Equal(t, "australiaeast", test2_output)
 	})
+
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
+	exists := azure.ResourceGroupExists(t, resourceGroupName, subscriptionId)
+	assert.True(t, exists, "Resource group does not exist")
 }
